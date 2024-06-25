437b832134632c9078e6e66ed3a6125f3d7be2f8
Valerio Cietto <valerio_ciet@hotmail.it>
2024-06-25 18:22:17 +0200

    Update testCombat.js

--- simulator/game-engine/testCombat.js
+++ simulator/game-engine/testCombat.js
@@ -5,8 +5,8 @@ simple combat, no combat tricks
 0/1 vs 0/1 no attack
 0/1 vs 0/x 
 1/1 vs 0/1 attack, no block
-
-
+2/2 vs 1/1 attack, no block
+1/1 attacks one mana open 1 card in hand, 1/1 no block
 
 */
 
